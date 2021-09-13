e28fd9fd664f7f1d077383b74cc1f119ace88937
Ryan Kelley <kelleyr@duvalschools.org>
2021-09-13 09:39:24 -0400

    [5b] v1.5

--- dataTypesVariables.java
+++ dataTypesVariables.java
@@ -1,4 +1,4 @@
-// Ryan Kelley, 09/13/21, Data Types and Variables v1.0
+// Ryan Kelley, 09/13/21, Data Types and Variables v1.5
 
 public class Main // This is a CLASS declaration.  
 {
@@ -7,6 +7,24 @@ public class Main // This is a CLASS declaration.
         System.out.println("What will print on the screen?");
         System.out.print("What will print on the screen?");
         System.out.print("  This is what will print on the screen.");
+
+
+        // PRIMITIVE Data Types & Variables 
+        // Java has STATIC TYPING: you must declare variables before use.
+        double time;  // This is a FLOATING POINT VALUE (has decimal). 
+        double avgGrade = 54.5; // Value ASSIGNED.  
+
+        boolean lightsOn; // This is a BOOLEAN VALUE, True or False.  
+        boolean isRaining = false; // Value ASSIGNED. 
+
+        int ageInYears; // This is an INTEGER (whole numbers, pos/neg.)
+        int speedLimit = 11222525; // Value ASSIGNED. 
+
+        String userName;  // This is a STRING (collection of characters.)
+        String streetName = "123 Sesamstraße"; // Value ASSIGNED. 
+
+        final int speedOfLight = 3000000; // Prevents variable from changing. 
+
     }
 
 }

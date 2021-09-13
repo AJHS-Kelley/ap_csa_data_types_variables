ba6a6e1fcb5c7cc5fd469c6ce4eb301a4e703243
Ryan Kelley <kelleyr@duvalschools.org>
2021-09-13 09:18:29 -0400

    [5B] v1.0
    Example of print vs. println

---
+++ dataTypesVariables.java
@@ -0,0 +1,12 @@
+// Ryan Kelley, 09/13/21, Data Types and Variables v1.0
+
+public class Main // This is a CLASS declaration.  
+{
+// Difference between println and print 
+    public static void main(String[] args) { // This is a METHOD declaration.
+        System.out.println("What will print on the screen?");
+        System.out.print("What will print on the screen?");
+        System.out.print("  This is what will print on the screen.");
+    }
+
+}
